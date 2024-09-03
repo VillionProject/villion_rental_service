1b3e3c7695f2d7dfc4d56a18c76c3d032063717e
unknown <lsi6601@gmail.com>
2024-09-03 18:01:18 +0900

    db ip 변경

--- src/test/java/org/example/villion_rental_service/VillionRentalServiceApplicationTests.java
+++ src/test/java/org/example/villion_rental_service/VillionRentalServiceApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.villion_rental_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class VillionRentalServiceApplicationTests {
 
     @Test
